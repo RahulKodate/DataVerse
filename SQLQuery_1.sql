4e5dadf9ef67a9d6484d543f707a9223d03de672
Rahul Ashok Kodate <115193023+RahulKodate@users.noreply.github.com>
2023-03-01 20:36:06 -0500

    first commit

---
+++ SQLQuery_1.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE DAMG6210
+
+use DAMG6210
+GO
+
+CREATE TABLE Customer (
+    CustomerID int not null,
+    CustomerName nvarchar(50),
+    CustomerAddress nvarchar(50),
+    CustomerCity varchar(20),
+    CustomerState char(2),
+    CustomerPostalCode varchar(9),
+    CONSTRAINT Customer_PK primary key (CustomerID)
+)
+CREATE TABLE [Order] 
+(
+    OrderID int not null,
+    OrderDate datetime default ( getdate()),
+    CustomerID int not null,
+    CONSTRAINT Order_PK primary key (OrderID),
+    CONSTRAINT Order_FK foreign key (CustomerID) references Customer(CustomerID)
+)
+CREATE TABLE Product (
+    ProductID int not null,
+    ProductDescription varchar(50),
+    ProductFinish varchar(20) CONSTRAINT ProductFinish_CK CHECK
+    (ProductFinish IN ('Cherry', 'Natural Ash', 'Red Oak')),
+    ProdcutStandardPrice decimal (6,2),
+    ProductLineID int 
+    CONSTRAINT Product_PK primary key (ProductID)
+)
+
+CREATE TABLE OrderLine (
+    OrderID int not null,
+    ProductID int not null,
+    OrderQuantity smallint,
+    CONSTRAINT OrderLine_PK primary key (OrderID,  ProductID),
+    CONSTRAINT OrderLine_FK1 foreign key (OrderID) references [Order] (OrderID),
+    CONSTRAINT OrderLine_FK2 foreign key (ProductID) references Product (ProductID)
+)
